b26825dd55d2ebb831d6d1695845b3fd852a4a50
Yuriy <the.whu@gmail.com>
2024-11-04 21:59:10 +0200

    Added @listID vaiadble to tests

--- cypress/e2e/integration/crud_lists.spec.cy.ts
+++ cypress/e2e/integration/crud_lists.spec.cy.ts
@@ -2,8 +2,6 @@
 import * as allure from "allure-js-commons";
 
 describe('ClickUp CRUD Tests for Lists', () => {
-    let listId;
-
     beforeEach(() => {
         // Create a new list before each test
         const uniqueName = `Test List ${Date.now()}`;
@@ -23,54 +21,60 @@ describe('ClickUp CRUD Tests for Lists', () => {
             },
         }).then((response) => {
             expect(response.status).to.equal(200);
-            listId = response.body.id;
+            cy.wrap(response.body.id).as('listId'); // Set alias here
         });
     });
 
     afterEach(() => {
         // Delete the list after each test
-        cy.request({
-            method: 'DELETE',
-            url: `${Cypress.env('BASE_URL')}/list/${listId}`,
-            headers: {
-                Authorization: Cypress.env('CLICKUP_API_TOKEN'),
-            },
-        }).then((response) => {
-            expect(response.status).to.equal(200);
+        cy.get('@listId').then((listId) => {
+            cy.request({
+                method: 'DELETE',
+                url: `${Cypress.env('BASE_URL')}/list/${listId}`,
+                headers: {
+                    Authorization: Cypress.env('CLICKUP_API_TOKEN'),
+                },
+            }).then((response) => {
+                expect(response.status).to.equal(200);
+            });
         });
     });
 
     it('should get the list', () => {
-        cy.request({
-            method: 'GET',
-            url: `${Cypress.env('BASE_URL')}/list/${listId}`,
-            headers: {
-                Authorization: Cypress.env('CLICKUP_API_TOKEN'),
-            },
-        }).then((response) => {
-            expect(response.status).to.equal(200);
-            expect(response.body.id).to.equal(listId);
+        cy.get('@listId').then((listId) => {
+            cy.request({
+                method: 'GET',
+                url: `${Cypress.env('BASE_URL')}/list/${listId}`,
+                headers: {
+                    Authorization: Cypress.env('CLICKUP_API_TOKEN'),
+                },
+            }).then((response) => {
+                expect(response.status).to.equal(200);
+                expect(response.body.id).to.equal(listId);
+            });
         });
     });
 
     it('should update the list', () => {
         const updatedName = `Updated List ${Date.now()}`;
-        cy.request({
-            method: 'PUT',
-            url: `${Cypress.env('BASE_URL')}/list/${listId}`,
-            headers: {
-                Authorization: Cypress.env('CLICKUP_API_TOKEN'),
-                'Content-Type': 'application/json',
-            },
-            body: {
-                name: updatedName,
-                content: 'This list has been updated',
-                due_date: null,
-                due_date_time: false,
-                priority: 2,
-            },
-        }).then((response) => {
-            expect(response.status).to.equal(200);
+        cy.get('@listId').then((listId) => {
+            cy.request({
+                method: 'PUT',
+                url: `${Cypress.env('BASE_URL')}/list/${listId}`,
+                headers: {
+                    Authorization: Cypress.env('CLICKUP_API_TOKEN'),
+                    'Content-Type': 'application/json',
+                },
+                body: {
+                    name: updatedName,
+                    content: 'This list has been updated',
+                    due_date: null,
+                    due_date_time: false,
+                    priority: 2,
+                },
+            }).then((response) => {
+                expect(response.status).to.equal(200);
+            });
         });
     });
 });
